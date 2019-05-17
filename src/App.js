acf2a6b355754f8432fa2c068111d68a5682c94c
Maël Gaonach <mael@gaonach.com>
2019-05-17 13:24:10 +0200

    refactor

--- src/App.js
+++ src/App.js
@@ -1,39 +1,31 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import adapter from "webrtc-adapter";
+
+const constraints = {
+  video: true
+};
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.initVideo();
-  }
-
-  initVideo() {
-    let video = (
-      <video
-        ref={video => {
-          this.video = video;
-        }}
-        autoPlay
-        muted
-      />
-    );
     this.state = {
-      video: video
+      videoRef: this.createVideoRef(constraints)
     };
-
-    navigator.mediaDevices
-      .getUserMedia({
-        video: true
-      })
-      .then(stream => {
-        this.video.srcObject = stream;
-      });
   }
+
+  createVideoRef(constraints) {
+    let ref = React.createRef();
+    navigator.mediaDevices.getUserMedia(constraints).then(stream => {
+      this.state.videoRef.current.srcObject = stream;
+    });
+    return ref;
+  }
+
+  initVideo() {}
 
   render() {
-    return this.state.video;
+    return <video ref={this.state.videoRef} autoPlay muted />;
   }
 }
 
