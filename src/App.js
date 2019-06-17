7c162db3885a91e222789248cb40987ff2f796b1
cucureuil <chloe.masoni@imt-atlantique.net>
2019-06-18 01:05:33 +0200

    tests structure

--- src/App.js
+++ src/App.js
@@ -9,18 +9,59 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      initiateur: null,
       peer: null,
-      stream: null
+      streamLocal: null
     };
   }
 
+  initAll() {
+    this.initVideoLocale();
+    let initiateur = new SimplePeer({
+      initiator: true,
+      stream: this.state.streamLocal,
+      trickle: false
+    });
+    let peer = new SimplePeer({
+      initiator: false,
+      stream: this.state.streamLocal,
+      trickle: false
+    });
+    initiateur.on("signal", function(data) {
+      console.log("Signal !");
+      /*
+       * Le SDP du peer local est généré ici, soit quand on clique sur "GENERATE" (dans ce cas c'est un SDP de type offer),
+       * sinon quand on rentre un SDP offer dans "FRIEND-ID" et que l'on clique sur "CONNECTION" (dans ce cas c'est un SDP de type answer)
+       */
+      document.querySelector("#offer").value = JSON.stringify(data);
+      peer.signal(data);
+    });
+
+    peer.on("signal", function(data) {
+      console.log("Signal !");
+      /*
+       * Le SDP du peer local est généré ici, soit quand on clique sur "GENERATE" (dans ce cas c'est un SDP de type offer),
+       * sinon quand on rentre un SDP offer dans "FRIEND-ID" et que l'on clique sur "CONNECTION" (dans ce cas c'est un SDP de type answer)
+       */
+      document.querySelector("#offer").value = JSON.stringify(data);
+      initiateur.signal(data);
+    });
+    peer.on("stream", function(stream) {
+      let video = document.querySelector("#receiver-video");
+      video.srcObject = stream;
+      video.play();
+    });
+  }
+
   bindEvents(p) {
+    console.log("BIND");
     p.on("error", function(err) {
       console.log("error", err);
     });
 
     // notifie le peerA quand le peerB envoie une donnée
     p.on("signal", function(data) {
+      console.log("Signal !");
       /*
        * Le SDP du peer local est généré ici, soit quand on clique sur "GENERATE" (dans ce cas c'est un SDP de type offer),
        * sinon quand on rentre un SDP offer dans "FRIEND-ID" et que l'on clique sur "CONNECTION" (dans ce cas c'est un SDP de type answer)
@@ -35,6 +76,7 @@ class App extends React.Component {
 
     // audio/video
     p.on("stream", function(stream) {
+      console.log("Stream !");
       let video = document.querySelector("#receiver-video");
       video.srcObject = stream;
       video.play();
@@ -42,6 +84,7 @@ class App extends React.Component {
 
     // chat
     p.on("data", function(data) {
+      console.log("data !");
       let chatBox = document.getElementById("messagesBox");
       let node = document.createElement("li");
       node.textContent = data.toString();
@@ -49,23 +92,13 @@ class App extends React.Component {
     });
   }
 
-  initLocalMedia() {
+  initVideoLocale() {
     navigator.getUserMedia(
       {
         video: true,
         audio: true
       },
       function(stream) {
-        let p = new SimplePeer({
-          initiator: true,
-          stream: stream,
-          trickle: false
-        });
-        this.setState({
-          peer: p,
-          stream: stream
-        });
-        this.bindEvents(this.state.peer);
         let emitterVideo = document.querySelector("#emitter-video");
         emitterVideo.srcObject = stream;
         emitterVideo.play();
@@ -74,45 +107,71 @@ class App extends React.Component {
     );
   }
 
-  initConnection(event) {
-    event.preventDefault();
-    if (this.state.peer == null) {
-      // eslint-disable-next-line
-      this.state.peer = new SimplePeer({
-        initiator: false,
-        trickle: false
-      });
-      this.bindEvents(this.state.peer);
+  initInitiator() {
+    console.log("Je suis initiateur !");
+    let initiateur = new SimplePeer({
+      initiator: true,
+      stream: this.state.streamLocal,
+      trickle: false
+    });
+    let peer = this.state.peer;
+    //this.bindEvents(initiateur);
+    initiateur.on("signal", function(data) {
+      console.log("Signal !");
+      /*
+       * Le SDP du peer local est généré ici, soit quand on clique sur "GENERATE" (dans ce cas c'est un SDP de type offer),
+       * sinon quand on rentre un SDP offer dans "FRIEND-ID" et que l'on clique sur "CONNECTION" (dans ce cas c'est un SDP de type answer)
+       */
+      document.querySelector("#offer").value = JSON.stringify(data);
+      peer.signal(data);
+    });
+    this.setState({
+      initiateur: initiateur
+    });
+  }
+
+  initPeer() {
+    console.log("Je suis un peer !");
+    let peer = new SimplePeer({
+      initiator: false,
+      stream: this.state.streamLocal,
+      trickle: false
+    });
+
+    //this.bindEvents(peer);
+    peer
+      .on("signal", function(data) {
+        console.log("Signal !");
+        /*
+         * Le SDP du peer local est généré ici, soit quand on clique sur "GENERATE" (dans ce cas c'est un SDP de type offer),
+         * sinon quand on rentre un SDP offer dans "FRIEND-ID" et que l'on clique sur "CONNECTION" (dans ce cas c'est un SDP de type answer)
+         */
+        document.querySelector("#offer").value = JSON.stringify(data);
+        this.state.initiator.signal(data);
+      })
+      .bind(this);
+    this.setState({
+      peer: peer
+    });
+  }
+
+  join(isInitiator) {
+    this.initAll();
+    // initialise la video locale
+    //this.initVideoLocale();
+    // initialise le peer
+    /**
+     *  il faut obligatoirement un initiateur et les autres peer
+     *  pour cela, dans l'url, l'initiateur ajours #1
+     **/
+
+    /*
+    if (isInitiator) {
+      this.initInitiator();
+    } else {
+      this.initPeer();
     }
-
-    this.state.peer.signal(JSON.parse(document.getElementById("form").value));
-  }
-
-  muteUnmute() {
-    if (this.state.stream != null)
-      this.state.stream.getAudioTracks()[0].enabled = !this.state.stream.getAudioTracks()[0]
-        .enabled;
-  }
-
-  hideVideo() {
-    if (this.state.stream != null)
-      this.state.stream.getVideoTracks()[0].enabled = !this.state.stream.getVideoTracks()[0]
-        .enabled;
-  }
-
-  sendMessage(event) {
-    event.preventDefault();
-    if (this.state.peer == null) {
-      console.log("NULL");
-      // eslint-disable-next-line
-      this.state.peer = new SimplePeer({
-        initiator: false,
-        trickle: false
-      });
-      this.bindEvents(this.state.peer);
-    }
-    let message = document.getElementById("message").value;
-    this.state.peer.send(message);
+    */
   }
 
   render() {
@@ -126,90 +185,20 @@ class App extends React.Component {
             <video id="receiver-video" />
           </div>
         </div>
-        <div className="row justify-content-center">
-          <SwitchBtn
-            name="p2p"
-            value={false}
-            turnOn={this.hideVideo.bind(this)}
-            turnOff={this.hideVideo.bind(this)}
-            imgOn={require("./img/callon.png")}
-            imgOff={require("./img/calloff.png")}
-          />
-          <SwitchBtn
-            name="video"
-            turnOn={this.hideVideo.bind(this)}
-            turnOff={this.hideVideo.bind(this)}
-            imgOn={require("./img/webcamoff.png")}
-            imgOff={require("./img/webcamon.png")}
-          />
-          <SwitchBtn
-            name="audio"
-            turnOn={this.muteUnmute.bind(this)}
-            turnOff={this.muteUnmute.bind(this)}
-            imgOn={require("./img/microoff.png")}
-            imgOff={require("./img/microoff.png")}
-          />
+
+        <div className="row">
+          <button id="create" onClick={() => this.join(true)}>
+            Create Conference !
+          </button>
+          <button id="join" onClick={() => this.join(false)}>
+            Join !
+          </button>
         </div>
         <div className="row">
           <div className="col-4 form-group">
             <label for="offer">MY-ID:</label>
             <textarea id="offer" className="form-control" />
-            <button
-              id="start"
-              className="btn btn-primary"
-              onClick={this.initLocalMedia.bind(this)}
-            >
-              GENERATE
-            </button>
-
-            <form id="incoming" onSubmit={this.initConnection.bind(this)}>
-              <label for="form">FRIEND-ID:</label>
-              <textarea id="form" className="form-control" />
-              <p>
-                <button type="submit" className="btn btn-primary">
-                  CONNECTION
-                </button>
-              </p>
-            </form>
           </div>
-          <div className="col-8">
-            <div className=" form-group">
-              <form id="messagesForm" onSubmit={this.sendMessage.bind(this)}>
-                <label for="message">MESSAGE:</label>
-                <textarea id="message" className="form-control" />
-                <button
-                  id="sendMessage"
-                  className="btn btn-primary"
-                  type="submit"
-                >
-                  SEND
-                </button>
-              </form>
-              <div>
-                <ul id="messagesBox" />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <p>
-            Instructions: pour demarrer une session vidéo, suivre les
-            instructions suivantes: // pour faciliter le tout, ouvrez des
-            fenêtre de votre navigateur
-          </p>
-          <p>
-            1- Cliquer sur générer pour générer votre ID et initialiser la
-            connexion.
-          </p>
-          <p>
-            2- Copier votre ID et donner le à votre peer pour qu'il l'insère
-            dans "FRIENDID" plus qu'il clique sur "CONNEXION"
-          </p>
-          <p>
-            3- L'ID de votre peer va être généré à ce moment là, il doit vous
-            envoyer son ID pour que vous l'insérez dans "FRIENDID" puis cliquez
-            sur "CONNEXION" et le tour est joué !
-          </p>
         </div>
       </div>
     );
