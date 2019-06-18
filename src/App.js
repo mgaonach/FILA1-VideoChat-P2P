26f48c911b55e558224c6ce51653f1b82c4f05d8
marem <marclamy.bzh@gmail.com>
2019-06-18 13:41:05 +0200

    UsernameForm signal channel connection

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import React from "react";
-import "react-bootstrap";
-import Routes from "./Routes/Routes"
+import React from 'react'
+import 'react-bootstrap'
 import { BrowserRouter } from 'react-router-dom'
+
+import { withSignalChannel } from './SignalChannel/SignalChannelProvider'
+
+import Routes from './Routes/Routes'
+import UsernameForm from './UsernameForm/UsernameForm'
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      showUsernameForm: false
+      usernameFormVisible: false
     };
+  }
+
+  showUsernameForm(){
+    this.setState({ usernameFormVisible: true });
+  }
+
+  hideUsernameForm(){
+    this.setState({ usernameFormVisible: false });
+  }
+
+  toggleUsernameForm = () => {
+    if ( this.state.usernameFormVisible ) {
+      this.hideUsernameForm();
+    } else {
+      this.showUsernameForm();
+    }
   }
 
   render() {
     return (
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
+      <React.Fragment>
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+
+        { this.state.usernameFormVisible ? <UsernameForm></UsernameForm> : ''}
+
+        <button onClick={ this.toggleUsernameForm }>Username</button>
+      </React.Fragment>
     );
   }
 }
 
-export default App;
+export default withSignalChannel(App);
+
